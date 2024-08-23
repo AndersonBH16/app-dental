d17f4617cd0f24e93479e293f6a900679ea8cf89
Anderson Blas <andi_1516@hotmail.com>
2024-08-23 02:04:33 -0500

    Setup upcube UI admin template

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,7 +15,8 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+//    Route::get('/dashboard', function () {
+//        return view('dashboard');
+//    })->name('dashboard');
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
 });
